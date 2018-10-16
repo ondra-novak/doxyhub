e3648b847a2f0670e150962c82212a9037c1f7bb
Ondřej Novák <ondra-novak@email.cz>
2018-10-16 13:24:08 +0200

    some animations and progressbars

--- www/console/index.js
+++ www/console/index.js
@@ -28,7 +28,53 @@ function start() {
 		sch.set(document.body, status)
 
 		sch.commit(status);
+
+		if (status == "queued" || status == "building") {
+			setProgress(null);
+		}
+		else if (status == "done") {
+			setProgress(100);
+		}
 	}
 	window.setStatus = setStatus;
+	
+	function setProgress(value, fake) {		
+		if (timer) {
+			clearTimeout(timer);
+			timer = null;
+		}
+		var allp = document.querySelectorAll("dh-progress > div");
+		if (value == null) {
+		allp.forEach(function(x){
+			x.style.width = "";
+			x.classList.add("unknown");
+		});
+
+		}else {
+			if (fake && value >=93) {
+				value = (1-Math.pow(0.75,value*0.1))*100;
+			}
+			allp.forEach(function(x){
+				x.style.width = value+"%";
+				x.classList.remove("unknown");
+			})
+		}
+	}
+
+
+	
+	var timer;
+	
+	function setProgressAutoTime(start_time, duration) {
+		
+		var now = Date.now();
+		var ellapsed = now - start_time;
+		var part = ellapsed*100.0/duration;
+		setProgress(part,true);
+		timer = setTimeout(setProgressAutoTime.bind(null,start_time,duration),Math.random()*5000);
+	}
+	
+	window.setProgress = setProgress;
+	window.setProgressAutoTime = setProgressAutoTime;
 	
 }
